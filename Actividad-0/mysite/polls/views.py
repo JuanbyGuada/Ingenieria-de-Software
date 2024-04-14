3973c76dd406af6b5b3c5cc10c648c00bdc45b2e
JuanbyGuada <jcasasortizderosas@alu.frlp.utn.edu.ar>
2024-04-14 03:31:35 -0300

    [Parte 4.1] Actualizacion de detail.html. Implementacion de votacion y resultados

--- Actividad-0/mysite/polls/views.py
+++ Actividad-0/mysite/polls/views.py
@@ -1,7 +1,9 @@
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -15,8 +17,23 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question":question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question=get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        # Muestra el formulario de votacion de nuevo
+        return render(request, "polls/detail.html", {
+            "question":question,
+            "error_message":"No seleccionaste una opcion.",
+        })
+    else:
+        selected_choice.votes=F("votes")+1
+        selected_choice.save()
+        # Siempre retornar un HttpResponseRedirect despues de tratar con exito
+        # los datos de un formulario POST. Esto previene que los datos sean
+        # posteados dos veces si un usuario presiona el boton de regreso.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
